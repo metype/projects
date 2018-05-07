25e58bad3e4fd03b0a44de26884392d673d726e6
Metype <39036543+metype@users.noreply.github.com>
2018-05-07 12:46:52 -0500

    Update evolution.js

--- evolution.js
+++ evolution.js
@@ -1,25 +1,13 @@
-var canvas = document.getElementById('canvas');
-
-        var engine = new BABYLON.Engine(canvas, true);
-
         var createScene = function(){
             var scene = new BABYLON.Scene(engine);
             scene.clearColor = new BABYLON.Color3.White();
-            var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0,
-            0,-10), scene);
-            camera.setTarget(BABYLON.Vector3.Zero());
-            camera.attachControl(canvas,true);
-            camera.keysUp.push(87);    //W
-            camera.keysDown.push(83)   //D
-            camera.keysLeft.push(65);  //A
-            camera.keysRight.push(68); //S
-
 
             var box = BABYLON.Mesh.CreateBox("Box",4.0,scene);
+            var camera = new BABYLON.ArcRotateCamera("arcCam",
+                    BABYLON.Tools.ToRadians(45),
+                    BABYLON.Tools.ToRadians(45),
+                    10.0,box.position,scene);
+            camera.attachControl(canvas,true);
+
             return scene;
         }
-
-        var scene = createScene();
-        engine.runRenderLoop(function(){
-            scene.render();
-        });
