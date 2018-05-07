6adbb1ede615d1c0319604ab1d315842297e53b1
Metype <39036543+metype@users.noreply.github.com>
2018-05-07 12:49:11 -0500

    Update evolution.js

--- evolution.js
+++ evolution.js
@@ -1,36 +1,25 @@
-        var canvas = document.getElementById('canvas');
+var canvas = document.getElementById('canvas');
 
         var engine = new BABYLON.Engine(canvas, true);
 
         var createScene = function(){
             var scene = new BABYLON.Scene(engine);
             scene.clearColor = new BABYLON.Color3.White();
+            var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0,
+            0,-10), scene);
+            camera.setTarget(BABYLON.Vector3.Zero());
+            camera.attachControl(canvas,true);
+            camera.keysUp.push(87);    //W
+            camera.keysDown.push(83)   //D
+            camera.keysLeft.push(65);  //A
+            camera.keysRight.push(68); //S
+
 
             var box = BABYLON.Mesh.CreateBox("Box",4.0,scene);
-
-            // Create a second object so we can actually witness the movement
-            // Make this one wireframe to distiguish the difference.
-            var box2 = BABYLON.Mesh.CreateBox("Box2",4.0,scene);
-            var material = new BABYLON.StandardMaterial("material1",scene);
-            material.wireframe = true;
-            box2.material = material;
-
-            box2.position = new BABYLON.Vector3(0,5,0);
-
-            var camera = new BABYLON.FollowCamera("followCam",BABYLON.Vector3.
-            Zero(),scene);
-            camera.target = box;
-            camera.radius = 100;
-            camera.heightOffset = 0;
-            camera.attachControl(canvas,true);
-
-            scene.activeCamera = camera;
             return scene;
         }
 
         var scene = createScene();
         engine.runRenderLoop(function(){
-            scene.getMeshByName("Box").position.y += 0.1;
-            scene.getMeshByName("Box").position.x += 0.1;
             scene.render();
         });
